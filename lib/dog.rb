a168039ca2167b6dc6aca3c76e1589fe281dd298
Miluche <118877950+Miluche@users.noreply.github.com>
2023-02-12 22:14:05 +0300

    Update dog.rb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
 
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed
+    @breed
+  end
 end
+
+dog = Dog.new("Umbwa Kali", "German Shepherd")
+puts dog.name
+puts dog.breed
+
+dog.name = "Ultimate Retriever"
+puts dog.name 
